27abc0b00092a545a33df9d1c30eb28dd60274ce
Danielddwp <48202417+Danielddwp@users.noreply.github.com>
2020-05-20 16:09:16 +0200

    :)

---
+++ PlainUDPSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace PlainUDPSender
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World press enter to start prepare sending environment!");
+            Console.ReadLine();
+            Random rvalue = new Random();
+            Measurement.Measurement measure;
+
+            UdpClient udpSender = new UdpClient(0);
+            udpSender.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 11111);
+
+            Console.WriteLine("ready to start press enter again");
+            Console.ReadLine();
+            string dateToday = DateTime.Now.Date.ToString();
+
+            while (true)
+            {
+                measure = new Measurement.Measurement(10, 15, 20);
+
+                byte[] sendBytes = Encoding.ASCII.GetBytes(measure.ToString());
+
+                try
+                {
+                    Console.WriteLine("sending :" + measure.ToString());
+                    udpSender.Send(sendBytes, sendBytes.Length, endPoint);
+                    Thread.Sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+        }
+    }
+}
---
+++ UdpReceiver/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UdpReceiver
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World receiver ready press enter!");
+            Console.ReadLine();
+            PostService postMeasure = new PostService();
+            Measurement.Measurement measurement;
+
+
+            UdpClient receiveUdp = new UdpClient(11111);
+
+            IPEndPoint receiveEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("ready to receive data");
+
+            try
+            {
+                while (true)
+                {
+                    Byte[] receiveBytes = receiveUdp.Receive(ref receiveEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("This message was sent from " +
+                                      receiveEndPoint.Address.ToString() +
+                                      " on their port number " +
+                                      receiveEndPoint.Port.ToString());
+
+                    Console.WriteLine(receivedData);
+
+                    string[] txtSplit = receivedData.Split(" ");
+                    for (int i = 0; i < txtSplit.Length; i++)
+                    {
+                        Console.WriteLine(txtSplit[i]);
+                    }
+
+                    measurement = new Measurement.Measurement(Convert.ToInt16(txtSplit[0]), Convert.ToInt16(txtSplit[1]) , Convert.ToDouble(txtSplit[2]) );
+                    postMeasure.PostItemHttpTask(measurement);
+                    Thread.Sleep(30000);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
